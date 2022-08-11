223de3ed4d644e43f7bb60f933044e00d89135e6
Vlatko Nikolovski <nikolovski.vlatko97@gmail.com>
2022-08-12 01:20:21 +0200

    Added Class 03 - Homework

---
+++ Class 03 - Homework/Class03Homework/Class03Homework/Controllers/BooksController.cs
@@ -0,0 +1,101 @@
+﻿using Class03Homework.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.Intrinsics.X86;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Class03Homework.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet] // http://localhost:[port]/api/books
+        public ActionResult<List<Book>> GetAllBooks()
+        {
+            try
+            {
+                return Ok(StaticDb.Books);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred! Contact the admin!");
+            }
+        }
+
+        [HttpGet("queryString")] //http://localhost:[port]/api/books/queryString?index=1
+        public ActionResult <Book> GetBookByIndex (int? index)
+        {
+            try
+            {
+                if (index < 0)
+                {
+                    return BadRequest("The index can not be negative!");
+                }
+
+                if (index >= StaticDb.Books.Count)
+                {
+                    return NotFound($"There is no resource on index {index}");
+                }
+
+                return Ok(StaticDb.Books[index.Value]);
+            }
+
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred! Contact the admin!");
+            }
+        }
+
+        [HttpGet("multipleQueryParams")]
+        public ActionResult<List<Book>> FilterBooks(string? author, string? title)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(author) && title == null)
+                {
+                    return BadRequest("Please enter at least one filter parameter!");
+                }
+
+                if (string.IsNullOrEmpty(author))
+                {
+                    List<Book> filteredBooks = StaticDb.Books.Where(x => x.Title.ToLower().Contains(title.ToLower())).ToList();
+                    return Ok(filteredBooks);
+                }
+
+                if (string.IsNullOrEmpty(title))
+                {
+                    List<Book> filteredBooks = StaticDb.Books.Where(x => x.Author.ToLower().Contains(author.ToLower())).ToList();
+                    return Ok(filteredBooks);
+                }
+
+                List<Book> booksDb = StaticDb.Books.Where(x => x.Author.ToLower().Contains(author.ToLower()) && (string)x.Title.ToLower() == title.ToLower()).ToList();
+
+                return Ok(booksDb);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred! Contact the admin!");
+            }
+        }
+
+        [HttpPost] 
+        public IActionResult AddNewBook([FromBody] Book book)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(book.Author) && string.IsNullOrEmpty(book.Title))
+                {
+                    return BadRequest("Book name and book author required!");
+                }
+
+                StaticDb.Books.Add(book);
+                return StatusCode(StatusCodes.Status201Created, "Book added");
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred! Contact the admin!");
+            }
+        }
+    }
+}
---
+++ Class 03 - Homework/Class03Homework/Class03Homework/StaticDb.cs
@@ -0,0 +1,14 @@
+﻿using Class03Homework.Models;
+
+namespace Class03Homework
+{
+    public static class StaticDb
+    {
+        public static List<Book> Books = new List<Book>()
+        {
+            new Book() { Author = "JK Rowling", Title = "Harry Potter" },
+            new Book() { Author = "F. Scott Fitzgerald", Title = "The Great Gatsby" },
+            new Book() { Author = "Arthur Conan Doyle", Title = "Sherlock Holmes" }
+        };
+    }
+}
